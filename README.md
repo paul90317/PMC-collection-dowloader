facbdf66ef503a4ce5991324674dc33f1bd73110
paul <paul2309825@gmail.com>
2022-04-30 10:48:48 +0800

    command

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 [discord 建立機器人](https://www.digitaltrends.com/gaming/how-to-make-a-discord-bot/)  
 [discord 機器人教學](https://hackmd.io/@kangjw/Discordpy%E6%A9%9F%E5%99%A8%E4%BA%BA%E5%BE%9E0%E5%88%B01%E8%B6%85%E8%A9%B3%E7%B4%B0%E6%95%99%E5%AD%B8)  
 [heroku worker](https://tousu.in/qa/?qa=1054926/)  
+[discord bot commands](https://betterprogramming.pub/how-to-make-discord-bot-commands-in-python-2cae39cbfd55)  
--- app.py
+++ app.py
@@ -1,42 +1,52 @@
+from posixpath import split
 import re
 import discord
 import io
 from code_getter import get_collection
 from dotenv import load_dotenv
 import os
+from discord.ext import commands
 
-client = discord.Client()
+bot = discord.Client()
 re_url=re.compile('https?:\/\/[a-zA-Z0-9\-]+(\.[a-zA-Z0-9]+)*(\/\S*)?')
 html_data=open('download.htm','r',encoding='utf-8').read()
 
 def io_get(urls:list):
     return io.BytesIO(html_data.replace('{urls}',str(urls)).encode('utf-8'))
 
-@client.event
-async def on_ready():
-    print('Login:', client.user)
-
-@client.event
+@bot.event
 async def on_message(message:discord.Message):
-    if message.author == client.user:
+    if message.author==bot.user:
         return
-    cmd=message.content
-    if not cmd:
+    data=message.content
+    if not data:
         return
-    cmd=cmd.split(' ')
-    if cmd[0] != 'cdl':
+    print(message.author,data)
+    data=data.split(' ')
+    cmd=data[0]
+    if cmd!='collection_download' and cmd!='cdl' :
+        print(True,"=> command pass")
         return
-    url=cmd[1]
+    if len(data)!=2:
+        print(False,"=> argument error")
+        return
+
+    url=data[1]
+
     if not re_url.fullmatch(url):
-        await message.channel.send(f'Please give me a link of inecraft planet collection.')
+        await message.channel.send(f'Please give me a link of minecraft planet collection.')
+        print(False,"=> not url")
         return
 
     try:
         cname=url.split('/')[5]
         await message.channel.send(f'Preparing the collection \"{cname}\" ...')
+        print(True,"=> crawl start ")
         dp,rp=get_collection(url)
+        
     except:
         await message.channel.send('This link is not minecraft planet collection :((')
+        print(False,"=> link is not minecraft planet collection ")
         return
 
     files=[]
@@ -44,7 +54,13 @@ async def on_message(message:discord.Message):
     files.append(discord.File(io_get(rp),filename='resourcepacks-download.htm'))
     content=f'Hello, the collection \"{cname}\" is prepared\n'
     await message.channel.send(content, files=files)
+    print(True,"=> crawl successfully")
 
 load_dotenv()
 
-client.run(os.getenv('TOKEN'))
+@bot.event
+async def on_ready():
+    id=os.getenv('ID')
+    print(f'https://discord.com/oauth2/authorize?client_id={id}&permissions=0&scope=bot%20applications.commands')
+
+bot.run(os.getenv('TOKEN'))
