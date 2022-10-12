d62e62bd0e147f4f496f81ec8eea6a7018135b1f
paul <paul2309825@gmail.com>
2022-10-12 16:40:09 +0800

    Change intents to default.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from get_code import collection_to_page
 from dotenv import load_dotenv
 import os
 
-bot = discord.Client()
+bot = discord.Client(intents=discord.Intents(messages=True,guilds=True))
 
 def io_get(data:str):
     return io.BytesIO(data.encode('utf-8'))
