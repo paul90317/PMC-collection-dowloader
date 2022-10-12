e48f40bcfbf15570d43df61ddf1cb420b1cb1a3e
paul <paul2309825@gmail.com>
2022-10-12 16:19:38 +0800

    Change intents to all

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from get_code import collection_to_page
 from dotenv import load_dotenv
 import os
 
-bot = discord.Client(intents=discord.Intents.default())
+bot = discord.Client(intents=discord.Intents.all())
 
 def io_get(data:str):
     return io.BytesIO(data.encode('utf-8'))
