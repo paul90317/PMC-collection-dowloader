c1398bdbac49d5e4a084509f3f782b388e78bf64
paul <paul2309825@gmail.com>
2022-10-13 20:37:11 +0800

    Fix intents

--- app.py
+++ app.py
@@ -5,34 +5,29 @@ from get_code import collection_to_page
 from dotenv import load_dotenv
 import os
 import nest_asyncio
-import crawl
 from parser import parser
 nest_asyncio.apply()
 
-bot = commands.Bot(command_prefix='/',intents=discord.Intents.default())
+bot = commands.Bot(command_prefix='/',intents=discord.Intents.all())
 
 def io_get(data:str):
     return io.BytesIO(data.encode('utf-8'))
 
 @bot.command()
 async def cdl(ctx:commands.context.Context, url='https://www.planetminecraft.com/collection/147813/hello-world/'):
-    print(f'[{ctx.author.name}] {url}')
-
+    print(f'[{ctx.author}] {url}')
     if not parser.collection(url):
-        await ctx.send('This is not a minecraft planet collection link :((')
+        await ctx.channel.send('This is not a minecraft planet collection link :((')
         print("[Debug] Crawl Error")
         return
 
-    cname=url.split('/')[5]
-    await ctx.send(f'Preparing the collection \"{cname}\" ...')
     print("[Debug] Crawl Start")
     data=collection_to_page(url)
 
-    files=[]
-    files.append(discord.File(io_get(data),filename='download.htm'))
-    content=f'Hello, the collection \"{cname}\" is prepared!\nRun download.htm below with chrome!'
+    files=[discord.File(io_get(data),filename='download.htm')]
+    content=f'{ctx.author.mention}, the collection `{url}` is prepared!\nRun `download.htm` below with **chrome**!'
     print("[Debug] Crawl Successfully")
-    await ctx.send(content, files=files)
+    await ctx.channel.send(content, files=files)
 
 @bot.event
 async def on_ready():
